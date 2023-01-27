b1957b5fbbc39f173df1ccc1e8b44cc0a392b859
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-27 19:52:56 +0300

    Задание 3 обновлено

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите любое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        double rez = Math.Pow(i, 3);
+        System.Console.WriteLine($"{i}^3={rez}");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено некорректное число");
+}
