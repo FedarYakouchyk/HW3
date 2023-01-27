8f8f91581670befcda97ccc1d24d71762b2ae40d
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-27 19:50:11 +0300

    Задание 1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите любое 5-ти значное число: ");
+string? num = Console.ReadLine();
+int size = Convert.ToInt32(Convert.ToString(num!.Length));
+int i = Convert.ToInt32(Convert.ToString(num[0]));
+if (size == 5)
+{
+
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        System.Console.WriteLine("Поздравляем, данное число является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Данное число не является палиндромом");
+    }
+}
+else
+{
+    System.Console.WriteLine("Повторите попытку, введённое вами число не является 5-ти значным.");
+}
